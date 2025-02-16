ead1cff0fc4bec9b76a7e673ce85e80492f39dc9
Mit Rajput <74498715+mitrajput19@users.noreply.github.com>
2025-02-16 17:28:31 +0530

    refactor BookingEvent to use abstract class and add AddBooking and GetBooking events

--- lib/bloc/booking_bloc/booking_event.dart
+++ lib/bloc/booking_bloc/booking_event.dart
@@ -1,4 +1,20 @@
 part of 'booking_bloc.dart';
 
-@immutable
-sealed class BookingEvent {}
+abstract class BookingEvent {
+  const BookingEvent();
+
+  @override
+  List<Object> get props => [];
+}
+
+class AddBooking extends BookingEvent {
+  final Booking booking;
+
+  const AddBooking(this.booking);
+
+  @override
+  List<Object> get props => [booking];
+}
+
+class GetBooking extends BookingEvent {
+}
