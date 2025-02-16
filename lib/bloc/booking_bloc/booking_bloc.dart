07085506a25a06c91a9582b3632e2559f42f3a48
Mit Rajput <74498715+mitrajput19@users.noreply.github.com>
2025-02-16 17:28:24 +0530

    implement BookingBloc event handlers for adding and retrieving bookings

--- lib/bloc/booking_bloc/booking_bloc.dart
+++ lib/bloc/booking_bloc/booking_bloc.dart
@@ -1,13 +1,37 @@
 import 'package:bloc/bloc.dart';
-import 'package:meta/meta.dart';
+
+import '../../models/booking/booking.dart';
+import '../../repositories/booking_repository.dart';
 
 part 'booking_event.dart';
 part 'booking_state.dart';
 
 class BookingBloc extends Bloc<BookingEvent, BookingState> {
-  BookingBloc() : super(BookingInitial()) {
-    on<BookingEvent>((event, emit) {
-      // TODO: implement event handler
-    });
+  final BookingRepository bookingRepository;
+
+  BookingBloc({required this.bookingRepository}) : super(BookingInitial()) {
+    on<AddBooking>(_onAddBooking);
+    on<GetBooking>(_getBooking);
+  }
+
+  Future<void> _onAddBooking(AddBooking event, Emitter<BookingState> emit) async {
+    emit(BookingLoading());
+    try {
+      await bookingRepository.addBooking(event.booking);
+      emit(BookingAdded());
+    } catch (e) {
+      emit(BookingError(e.toString()));
+    }
+  }
+
+
+  Future<void> _getBooking(GetBooking event, Emitter<BookingState> emit) async {
+    emit(GetBookingLoading());
+    try {
+      final bookings = await bookingRepository.getBookings();
+      emit(GetBookingLoaded(bookings));
+    } catch (e) {
+      emit(GetBookingError(e.toString()));
+    }
   }
 }
