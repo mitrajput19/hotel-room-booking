8fe60e6ad33f38668171fd850ecc96c35299e75d
Mit Rajput <74498715+mitrajput19@users.noreply.github.com>
2025-02-16 17:28:36 +0530

    refactor BookingState to use abstract class and add additional states for loading, added, and error handling

--- lib/bloc/booking_bloc/booking_state.dart
+++ lib/bloc/booking_bloc/booking_state.dart
@@ -1,6 +1,41 @@
 part of 'booking_bloc.dart';
 
-@immutable
-sealed class BookingState {}
+abstract class BookingState {
+  const BookingState();
 
-final class BookingInitial extends BookingState {}
+  @override
+  List<Object> get props => [];
+}
+
+class BookingInitial extends BookingState {}
+
+class BookingLoading extends BookingState {}
+
+class BookingAdded extends BookingState {}
+
+class BookingError extends BookingState {
+  final String message;
+
+  const BookingError(this.message);
+
+  @override
+  List<Object> get props => [message];
+}
+
+class GetBookingLoading extends BookingState {}
+class GetBookingLoaded extends BookingState {
+  final List<Booking> bookings;
+
+  const GetBookingLoaded(this.bookings);
+
+  @override
+  List<Object> get props => [bookings];
+}
+class GetBookingError extends BookingState {
+  final String message;
+
+  const GetBookingError(this.message);
+
+  @override
+  List<Object> get props => [message];
+}
